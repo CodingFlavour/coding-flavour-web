eaa780db6f6d93a4cad1a5d409dde38b16786fac
DSanchez <danielsanchezpro@gmail.com>
2023-09-23 13:26:22 +0200

    @Components @Correct prettier configuration and extending to monorepo projects as a single common line

--- storybook/stories/components/Value/Value.stories.ts
+++ storybook/stories/components/Value/Value.stories.ts
@@ -3,19 +3,19 @@ import growthIcon from "../../../../web/src/presentation/assets/icons/growth.png
 import Value from "../../../../web/src/presentation/components/Value/Value";
 
 const meta: Meta<typeof Value> = {
-  component: Value,
+    component: Value,
 };
 
 export default meta;
 type Story = StoryObj<typeof Value>;
 
 export const DefaultValue: Story = {
-  args: {
-    value: {
-      id: "Test Value",
-      title: "Title Value",
-      description: "Description Value",
-      image: growthIcon,
+    args: {
+        value: {
+            id: "Test Value",
+            title: "Title Value",
+            description: "Description Value",
+            image: growthIcon,
+        },
     },
-  },
 };
