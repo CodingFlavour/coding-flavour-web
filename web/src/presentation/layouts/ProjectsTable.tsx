33409e2a24ce1758a6b55635eb12d65e699ac140
DSanchez <danielsanchezpro@gmail.com>
2023-10-19 21:13:43 +0200

    Checkpoint

--- web/src/presentation/layouts/ProjectsTable.tsx
+++ web/src/presentation/layouts/ProjectsTable.tsx
@@ -4,12 +4,16 @@ import Image from "next/image";
 import IconArrowUpRight from "@public/icons/icon-arrow-up-right.svg";
 import { IProject } from "@src/data/Models/Project";
 import Component from "@src/data/Models/Component";
+import Visit from "../components/Visit";
 
 export interface IProjectsTableProps {
   projects: IProject[];
 }
 
 const {
+  projectsTable,
+  projectsTable__wrapper,
+  projectsTable__header,
   project__row,
   project__row__counter,
   project__row__name,
@@ -18,41 +22,51 @@ const {
 
 const ProjectsTable: Component<IProjectsTableProps> = ({ projects, dict }) => {
   return (
-    <section data-testid={"projects-table"}>
-      {projects.map((project, index) => (
-        <div
-          className={project__row}
-          key={`row_${project.projectName.split(" ").join("-")}`}
-        >
-          <span
-            className={project__row__counter}
-            data-testid={`projects-table-${index}-counter`}
+    <section
+      className={projectsTable}
+      data-testid={"projects-table"}
+    >
+      <div className={`${projectsTable__wrapper} column_1`} data-testid={'projects-table-wrapper'}>
+        <h3 className={projectsTable__header} data-testid={'projects-table-header'}>{dict.ourProjects}</h3>
+        {projects.map((project, index) => (
+          <div
+            className={project__row}
+            key={`row_${project.projectName.split(" ").join("-")}_${index}`}
           >
-            {(index + 1).toString().padStart(3, "0")}
-          </span>
-          <span
-            className={project__row__name}
-            data-testid={`projects-table-${index}-name`}
-          >
-            {project.projectName}
-          </span>
-          <span
-            className={only_desktop}
-            data-testid={`projects-table-${index}-platform`}
-          >
-            {project.platform}
-          </span>
-          <span
-            className={only_desktop}
-            data-testid={`projects-table-${index}-date`}
-          >
-            {project.date}
-          </span>
-          <a href={project.url} data-testid={`projects-table-${index}-link`}>
-            <Image src={IconArrowUpRight} alt={`${dict.visit} ${project.url}`} />
-          </a>
-        </div>
-      ))}
+            <span
+              className={project__row__counter}
+              data-testid={`projects-table-${index}-counter`}
+            >
+              {(index + 1).toString().padStart(3, "0")}
+            </span>
+            <span
+              className={project__row__name}
+              data-testid={`projects-table-${index}-name`}
+            >
+              {project.projectName}
+            </span>
+            <span
+              className={only_desktop}
+              data-testid={`projects-table-${index}-platform`}
+            >
+              {project.platform}
+            </span>
+            <span
+              className={only_desktop}
+              data-testid={`projects-table-${index}-date`}
+            >
+              {project.date}
+            </span>
+            <a href={project.url} data-testid={`projects-table-${index}-link`}>
+              <Image
+                src={IconArrowUpRight}
+                alt={`${dict.visit} ${project.url}`}
+              />
+            </a>
+          </div>
+        ))}
+        <Visit href="/projects" text={dict.seeMore as string} />
+      </div>
     </section>
   );
 };
