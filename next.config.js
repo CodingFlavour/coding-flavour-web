a879a77110430e170f5c029269d05b12a6e1376d
DSanchez <danielsanchezpro@gmail.com>
2023-10-25 17:32:08 +0200

    @Functionalities @Changed all pages styles for correct spacing with Figma layout vars

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   sassOptions: {
     prependData: `
       @use "@src/presentation/styles/base/grid-system.scss" as gridSystem;
+      @use "@src/presentation/styles/base/spacing.scss" as spacing;
       @use "@src/presentation/styles/base/typography.scss" as typography;
       @use "@src/presentation/styles/base/animations.scss" as animations;
       @use "@src/presentation/styles/utilities/font-weight.scss" as fontWeight;
